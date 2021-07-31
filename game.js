22785934ce6d1f8503a2580847b465b4e5787ce0
cuonggio003 <nguyenquoccuongvntn2003@gmail.com>
2021-07-31 15:06:01 +0700

    case study

---
+++ game.js
@@ -0,0 +1,19 @@
+let rabbit;
+
+function setup() {
+    createCanvas(WITDH, HEIGHT);
+    newGame();
+}
+
+function draw() {
+    background(0);
+    drawrabbit);
+}
+
+function drawrabbit() {
+    rabbit.show();
+}
+
+function newGame() {
+    rabbit = new rabbit();
+}
